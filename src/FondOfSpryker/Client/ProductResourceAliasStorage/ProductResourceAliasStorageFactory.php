6a898815a9596ec6eb0a2cf4f7880b22f87d7b7f
Julian Zimmermann <julian@jzit.de>
2022-09-06 15:07:49 +0200

    prepare for spryker ugprade

--- src/FondOfSpryker/Client/ProductResourceAliasStorage/ProductResourceAliasStorageFactory.php
+++ src/FondOfSpryker/Client/ProductResourceAliasStorage/ProductResourceAliasStorageFactory.php
@@ -15,7 +15,6 @@ class ProductResourceAliasStorageFactory extends SprykerProductResourceAliasStor
         return new ProductConcreteStorageBySkuReader(
             $this->getStorageClient(),
             $this->getSynchronizationService(),
-            $this->getStore()
         );
     }
 }
